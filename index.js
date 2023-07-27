e45f0845dd4f88a7250219fa19c36cbc1c4552bf
ishanchaskar <91770647+ishanchaskar@users.noreply.github.com>
2023-07-27 22:52:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import { Form } from 'antd'
+import React from 'react'
+import  {Link} from 'react-router-dom'
+function Login(){
+
+    const onFinish=(values)=>{
+        console.log(values)
+    }
+    return(
+        <div className="flex justify-center items-center h-screen w-screen">
+            <div className="card w-400 p-3">
+                <div className="flex flex-col"></div>
+               
+                <h1 className="text-2xl">Login
+                <div className="divider"></div>
+                <Form layout="vertical" className="mt-2" onFinish={onFinish}>
+                    <Form.Item name='email' label='Email'>  
+                        <input type="text" placeholder="Username"/>
+                    </Form.Item>
+                    <Form.Item name='password' label='Password'>  
+                        <input type="password" placeholder="Password"/>
+                    </Form.Item>
+                    <div className="flex flex-col gap-2">
+                    <button type="submit" className="primary-contained-btn mt-2 w-100">Login</button>
+                    <Link className="a" to="/register">Register</Link>
+                    </div>
+                </Form>
+                </h1>
+            </div>
+        </div>
+    )
+}
+export default Login
